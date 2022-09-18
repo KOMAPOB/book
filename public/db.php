a9d65471de7d56da5c2d06930b262667d15df0fd
KOMAPOB <kom.semyon@mail.ru>
2022-09-18 19:00:18 +0500

    first commit

---
+++ public/db.php
@@ -0,0 +1,10 @@
+<?php
+    $dbhost = "localhost";
+    $bdname = "book-bd";
+    $username = "root";
+    $password = "";
+
+    $db = new PDO("mysql:host=$dbhost; dbname=$dbname", $username, $password);
+
+    
+?>
